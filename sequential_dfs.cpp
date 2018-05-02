545fabb2b11152979911bf9eccd233711fcf6374
divyanshu-talwar <divyanshu15028@iiitd.ac.in>
2018-05-02 23:32:59 +0530

    final commit

--- sequential_dfs.cpp
+++ sequential_dfs.cpp
@@ -30,19 +30,17 @@ graph* read_data(string file) {
 	fin >> rows >> columns >> nonzeros;
 	for(int line = 0; line < nonzeros; line++) {
 		int i, j, value;
-		fin >> i >> j >> value;
+		fin >> i >> j >> value;		
 		dataset->row.push_back(i - 1);
-		dataset->column.push_back(j - 1);
+		dataset->column.push_back(j - 1);		
 		dataset->data.push_back(value);
 	}
-	int start = -1;
+
+	int start = 0;
 	for(int i = 0; i < rows; i++) {
-		for(vi::iterator it = dataset->column.begin() + start; it != dataset->column.end(); it++) {
+		for(vi::iterator it = dataset->column.begin() + start; it != dataset->column.end(); it += 1) {
 			if( *it == i ) {
 				start = it - dataset->column.begin();
-				break;
-			}
-			else if( *it > i ) {
 				break;
 			}
 		}
@@ -61,7 +59,7 @@ void DFS_VISIT(graph* dataset_graph, int vertex, int* color, int* parents, int* 
 	time_dfs += 1;
 	discovery_time[vertex] = time_dfs;
 	color[vertex] = 1;
-	for(vi::iterator it = dataset_graph->dataset->column.begin() + dataset_graph->dataset->index_column[vertex]; *it != vertex; it++ ) {
+	for(vi::iterator it = dataset_graph->dataset->column.begin() + dataset_graph->dataset->index_column[vertex]; *it == vertex; it++ ) {
 		int neighbor_index = it - dataset_graph->dataset->column.begin();
 		int neighbor_vertex = dataset_graph->dataset->row[neighbor_index];
 		if( color[neighbor_vertex] == 0 ) {
@@ -77,7 +75,7 @@ void DFS_VISIT(graph* dataset_graph, int vertex, int* color, int* parents, int* 
 void DFS(graph* dataset_graph, int* color, int* parents, int* discovery_time, int* finish_time) {
 	time_dfs = 0;
 	for(int i = 0; i < dataset_graph->vertices; i++) {
-		if( color[dataset_graph->vertices] == 0 ) {
+		if( color[i] == 0 ) {
 			DFS_VISIT(dataset_graph, i, color, parents, discovery_time, finish_time);
 		}
 	}
@@ -86,14 +84,18 @@ void DFS(graph* dataset_graph, int* color, int* parents, int* discovery_time, in
 
 int main() {
 
-	string filename = "./../Dataset/fl2010.mtx";
+	// string filename = "./../Dataset/fl2010.mtx";
+	string filename = "./../Dataset/testcase.mtx";
+	// const char* filename = "./../Dataset/Stranke94.mtx";
+	// string filename = "./../Dataset/cage3.mtx";
+
 	// cout << "Please provide the dataset filename : ";
 	// cin >> filename;
 	
+	clock_t begin = clock();
 	graph* dataset_graph = read_data(filename);
 	printf("Generated the csc matrix!\n");
 
-	clock_t begin = clock();
 
 	// 0 -> White, 1 -> Gray, 2 -> Black
 	int* color = new int[dataset_graph->vertices];
@@ -113,6 +115,11 @@ int main() {
 
 	double elapsed_time = double(end - begin) / CLOCKS_PER_SEC;
 	printf("Sequential code took %.3f sec for execution.\n", elapsed_time);
+
+	for(int i = 0; i < dataset_graph->vertices; i++) {
+		printf("Discovery of %d - %d\n", i, discovery_time[i]);
+		printf("Finish of %d - %d\n", i, finish_time[i]);
+	}
 
 	return 0;
 
